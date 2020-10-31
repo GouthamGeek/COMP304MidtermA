ca890d4604d197905e6915b57c17fbc0b101c135
GouthamGeek <Goutham.pga@gmail.com>
2020-10-31 08:43:24 -0400

    Initial Commit

---
+++ app/src/main/java/goutham/moorthy/s300948360/GouthamActivity.java
@@ -0,0 +1,14 @@
+package goutham.moorthy.s300948360;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GouthamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
